d53abe56e55513a74a67d2d973b6299aab7a02dc
PoPoPo3 <60116442+PoPoPo3@users.noreply.github.com>
2021-04-08 17:00:42 +0300

    Finish

--- js/main.js
+++ js/main.js
@@ -4,6 +4,23 @@ $(function() {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        arrows: false
+        arrows: false,
+        responsive: [{
+            breakpoint: 1175,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            },
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }]
     });
+
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+
 });
